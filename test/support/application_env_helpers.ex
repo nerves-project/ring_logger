f8aaa7233bcda8a15327e911a094fa769792c7c0
Jason Axelson <jason.axelson@gmail.com>
2021-11-29 07:45:11 -1000

    Merge defaults from old configuration
    But log a warning when it happens

---
+++ test/support/application_env_helpers.ex
@@ -0,0 +1,27 @@
+defmodule RingLogger.ApplicationEnvHelpers do
+  @doc """
+  Set application env for one test, then resets it after
+
+  Add the following tag to set the configuration for `:ring_logger`:
+
+  ```
+  @tag application_envs: [ring_logger: [colors: %{debug: :green, error: :blue}]]
+  ```
+  """
+  def with_application_env(%{application_envs: application_envs} = context, on_exit) when is_function(on_exit, 1) do
+    if context.async, do: raise("Not compatible with `async: true`")
+
+    for {app, envs} <- application_envs do
+      original_envs = Application.get_all_env(app)
+      Application.put_all_env([{app, envs}])
+
+      on_exit.(fn ->
+        # We need to delete all the existing env because `Application.put_all_env` does a deep merge
+        for {key, _val} <- Application.get_all_env(app), do: Application.delete_env(app, key)
+        Application.put_all_env([{app, original_envs}])
+      end)
+    end
+  end
+
+  def with_application_env(_context, _on_exit), do: :ok
+end
