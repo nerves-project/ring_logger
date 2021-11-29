3f1d6c09e43d5994e730451d8c12b22aa2a90446
Jason Axelson <jason.axelson@gmail.com>
2021-11-29 07:52:17 -1000

    rewrite put_all_env to support elixir < 1.9.0

--- test/support/application_env_helpers.ex
+++ test/support/application_env_helpers.ex
@@ -14,15 +14,23 @@ defmodule RingLogger.ApplicationEnvHelpers do
 
     for {app, envs} <- application_envs do
       original_envs = Application.get_all_env(app)
-      Application.put_all_env([{app, envs}])
+      put_all_env([{app, envs}])
 
       on_exit.(fn ->
         # We need to delete all the existing env because `Application.put_all_env` does a deep merge
         for {key, _val} <- Application.get_all_env(app), do: Application.delete_env(app, key)
-        Application.put_all_env([{app, original_envs}])
+        put_all_env([{app, original_envs}])
       end)
     end
   end
 
   def with_application_env(_context, _on_exit), do: :ok
+
+  # `Application.put_all_env/2` is not availble until Elixir 1.9
+  defp put_all_env(application_envs) do
+    for {app, envs} <- application_envs,
+        {key, val} <- envs do
+      Application.put_env(app, key, val)
+    end
+  end
 end
