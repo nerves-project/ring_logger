e613f41dcb63764ae35fb1fe7c01afd54e7ae0d1
Frank Hunleth <fhunleth@troodon-software.com>
2023-02-07 08:26:19 -0500

    Require Elixir 1.9 and later; update CI
    Previous versions still work, but from this point on, we won't support
    issues with them. This also allows us to use functions that have been
    added to the language since RingLogger was originally implemented.

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule RingLogger.MixProject do
     [
       app: :ring_logger,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
