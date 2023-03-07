a7c362d993687b27414efa4e372bf9f6624ce79b
Frank Hunleth <fhunleth@troodon-software.com>
2023-03-06 22:06:27 -0500

    Bump minimum supported Elixir version due to Logger update

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule RingLogger.MixProject do
     [
       app: :ring_logger,
       version: @version,
-      elixir: "~> 1.9",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
