9e4a0e720c8cd40bc50d860f6d92aa6147795cbe
Suraj Borate <Suraj_Borate@onwardgroup.com>
2025-01-08 15:38:01 +0530

    FAQM-1585-Test cases description modified

--- test/ring_logger/viewer_test.exs
+++ test/ring_logger/viewer_test.exs
@@ -30,66 +30,67 @@ defmodule RingLogger.ViewerTest do
   end
 
   test "Ringlogger.view/1 with multiple commands ; separated", %{state: _state} do
-    Logger.debug("blofeld debug level sample msg", application: :blofeld_firmware)
-    Logger.warning("blofeld warn level sample msg", application: :telit_modem)
-    Logger.info("blofeld info level sample msg", application: :peridiod)
-    Logger.critical("blofeld critical level sample msg", application: nil)
-    Logger.emergency("blofeld emergency level sample msg", application: :Vintage_Net)
+    Logger.debug("Ringlogger debug level sample msg", application: :Ringlogger_firmware)
+    Logger.warning("Ringlogger warn level sample msg", application: :telit_modem)
+    Logger.info("Ringlogger info level sample msg", application: :peridiod)
+    Logger.critical("Ringlogger critical level sample msg", application: nil)
+    Logger.emergency("Ringlogger emergency level sample msg", application: :Vintage_Net)
     Logger.debug("---------enter q to exit out of given test-----", application: :User)
 
     assert :ok = RingLogger.Viewer.view("a telit_modem;l debug; q")
   end
 
   test "Ringlogger.view/1 with invalid command format", %{state: _state} do
-    Logger.debug("blofeld debug level sample msg", application: :blofeld_firmware)
-    Logger.warning("blofeld warn level sample msg", application: :telit_modem)
-    Logger.info("blofeld info level sample msg", application: :peridiod)
-    Logger.critical("blofeld critical level sample msg", application: nil)
-    Logger.emergency("blofeld emergency level sample msg", application: :Vintage_Net)
+    Logger.debug("Ringlogger debug level sample msg", application: :Ringlogger_firmware)
+    Logger.warning("Ringlogger warn level sample msg", application: :telit_modem)
+    Logger.info("Ringlogger info level sample msg", application: :peridiod)
+    Logger.critical("Ringlogger critical level sample msg", application: nil)
+    Logger.emergency("Ringlogger emergency level sample msg", application: :Vintage_Net)
     Logger.info("---------enter q to exit out of given test-----", application: :User)
 
     # observe in logs you wont find (debug) anywhere writtem at the bottom left corner
     assert :ok = RingLogger.Viewer.view("r l debug; q")
   end
 
+
   test "a command with single application", %{state: _state} do
-    Logger.debug("blofeld debug level sample msg", application: :blofeld_firmware)
-    Logger.warning("blofeld warn level sample msg", application: :telit_modem)
-    Logger.info("blofeld info level sample msg", application: :peridiod)
-    Logger.critical("blofeld critical level sample msg", application: nil)
-    Logger.emergency("blofeld emergency level sample msg", application: :Vintage_Net)
+    Logger.debug("Ringlogger debug level sample msg", application: :Ringlogger_firmware)
+    Logger.warning("Ringlogger warn level sample msg", application: :telit_modem)
+    Logger.info("Ringlogger info level sample msg", application: :peridiod)
+    Logger.critical("Ringlogger critical level sample msg", application: nil)
+    Logger.emergency("Ringlogger emergency level sample msg", application: :Vintage_Net)
     Logger.debug("---------enter q to exit out of given test-----", application: :User)
 
     assert :ok = RingLogger.Viewer.view("a telit_modem; q")
   end
 
   test "a command with multiple applications", %{state: _state} do
-    Logger.debug("blofeld debug level sample msg",
-      application: :blofeld_firmware,
+    Logger.debug("Ringlogger debug level sample msg",
+      application: :Ringlogger_firmware,
       time: 1_735_140_308_331_070
     )
 
-    Logger.warning("blofeld warn level sample msg", application: :telit_modem)
-    Logger.info("blofeld info level sample msg", application: :peridiod)
-    Logger.critical("blofeld critical level sample msg", application: nil)
-    Logger.emergency("blofeld emergency level sample msg", application: :Vintage_Net)
+    Logger.warning("Ringlogger warn level sample msg", application: :telit_modem)
+    Logger.info("Ringlogger info level sample msg", application: :peridiod)
+    Logger.critical("Ringlogger critical level sample msg", application: nil)
+    Logger.emergency("Ringlogger emergency level sample msg", application: :Vintage_Net)
     Logger.info("---------enter q to exit out of given test-----", application: :User)
 
-    assert :ok = RingLogger.Viewer.view("a nil blofeld_firmware peridiod; q")
+    assert :ok = RingLogger.Viewer.view("a nil Ringlogger_firmware peridiod; q")
   end
 
-  test "goto date command with Invalid arguments using Ringlogger.view/1 command", %{
+ test "goto date command with Invalid arguments using Ringlogger.view/1 command", %{
     state: _state
   } do
-    Logger.debug("blofeld debug level sample msg",
-      application: :blofeld_firmware,
+    Logger.debug("Ringlogger debug level sample msg",
+      application: :Ringlogger_firmware,
       time: 1_735_160_108_000_011
     )
 
-    Logger.warning("blofeld warn level sample msg", application: :telit_modem)
-    Logger.info("blofeld info level sample msg", application: :peridiod)
-    Logger.critical("blofeld critical level sample msg", application: nil)
-    Logger.emergency("blofeld emergency level sample msg", application: :Vintage_Net)
+    Logger.warning("Ringlogger warn level sample msg", application: :telit_modem)
+    Logger.info("Ringlogger info level sample msg", application: :peridiod)
+    Logger.critical("Ringlogger critical level sample msg", application: nil)
+    Logger.emergency("Ringlogger emergency level sample msg", application: :Vintage_Net)
 
     Logger.info("---------enter q to exit out of given test-----",
       application: :User,
@@ -100,15 +101,15 @@ defmodule RingLogger.ViewerTest do
   end
 
   test "goto date command with valid arguments using Ringlogger.view/1 command", %{state: _state} do
-    Logger.debug("blofeld debug level sample msg",
-      application: :blofeld_firmware,
+    Logger.debug("Ringlogger debug level sample msg",
+      application: :Ringlogger_firmware,
       time: 1_735_160_108_000_011
     )
 
-    Logger.warning("blofeld warn level sample msg", application: :telit_modem)
-    Logger.info("blofeld info level sample msg", application: :peridiod)
-    Logger.critical("blofeld critical level sample msg", application: nil)
-    Logger.emergency("blofeld emergency level sample msg", application: :Vintage_Net)
+    Logger.warning("Ringlogger warn level sample msg", application: :telit_modem)
+    Logger.info("Ringlogger info level sample msg", application: :peridiod)
+    Logger.critical("Ringlogger critical level sample msg", application: nil)
+    Logger.emergency("Ringlogger emergency level sample msg", application: :Vintage_Net)
 
     Logger.info("---------enter q to exit out of given test-----",
       application: :User,
